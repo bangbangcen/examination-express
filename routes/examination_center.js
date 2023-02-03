9ec11cc33d4f67706ec981282551c6a672c17e34
Tintin-PLUS <qinghuatintin@163.com>
2023-02-03 20:09:07 +0800

    体检中心表分页功能

--- routes/examination_center.js
+++ routes/examination_center.js
@@ -3,21 +3,27 @@ const router = express.Router();
 const db = require('../db/connector');
 const { ExaminationCenter } = require('../db/baseApi');
 
-router.get("/list", async (req, res) => {
-    const result = await ExaminationCenter.list();
-    var i=0;
-    for(var x in result.rows){
-      var d_id=result.rows[i].districtId;
-      const sql = `select name from district where id=$1`;
-      let result2 = await db.query(sql,[d_id]);
-      result.rows[i].districtId=result2.rows[0].name;
-      i++;
-    }
-    res.send(result.rows);
+router.post("/list", async (req, res) => {
+  const {curPage} = req.body;
+  const sql = `select 
+              a.name as n, district_id, address,nearby,telephone,announcement,latitude,longitude,morning_limit,afternoon_limit,b.name
+              from examination_center a
+              join district b 
+              on a.district_id=b.id
+              limit $1 , $2`;
+  let result = await db.query(sql,[(curPage-1)*5,curPage*5]);
+  res.send(result.rows);
 });
+
 router.post("/del", async (req, res) => {
   const {name} = req.body;
   const sql = `delete from examination_center where name = $1`;
   return db.query(sql, [name]);
+});
+
+router.post("/count", async (req, res) => {
+  const sql = `select count(*) from examination_center`;
+  let result = await db.query(sql);
+  res.send(result.rows[0].count);
 });
   module.exports = router;
