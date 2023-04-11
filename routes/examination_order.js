b720a5ae97cffabf84af9e6a22205c8935d16223
Tintin-PLUS <qinghuatintin@163.com>
2023-04-11 08:38:07 +0800

    Revert "Update examination_order.js"
    This reverts commit 034c2037a947e315aa5e3788f38377e249dae021.

--- routes/examination_order.js
+++ routes/examination_order.js
@@ -12,19 +12,16 @@ router.post("/list", async (req, res) => {
                     d.name as center_name,
                     a.examine_date as examine_date,
                     a.status as status,
-                    a.type as type,
-                    e.company_name as company_name
+                    a.type as type
                 from 
                     examination_order a,
                     examinee b,
                     package c,
-                    examination_center d,
-                    group_information e
+                    examination_center d 
                 where
                     a.examinee_id=b.id and
                     a.package_id=c.id and
                     a.center_id=d.id and
-                    nvl(a.group_information_id,'0')=e.id and
                     CONCAT(b.name, b.phone) LIKE $1` 
                     ;
     let result = await db.query(sql,["%"+dname+"%"]);
